36a8f8f19e8379c06f760388c038fe9d995b63f1
Ojo Adebayo <bayoojo1@yahoo.com>
2021-08-04 16:47:22 +0100

    wordlenght

---
+++ prompt.js
@@ -0,0 +1,7 @@
+let firstWord = prompt("Write a word");
+let secondWord = prompt("Write another word");
+
+let totalWord = firstWord + secondWord;
+
+
+alert("The string " + firstWord + " and " + secondWord + " are " + totalWord.length + " characters in length");
